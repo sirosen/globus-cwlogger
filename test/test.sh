66216b3fdf613c49d4c40c87ff678059276b08e7
aaschaer <aaschaer@uchicago.edu>
2018-04-03 11:54:09 -0400

    update tests to allow specifying test branch

--- test/test.sh
+++ test/test.sh
@@ -3,6 +3,15 @@
 
 #!/bin/bash
 
+# get a branch to test or default to master
+if [ $# -eq 1 ]
+then
+    branch=$1
+else
+    # default to master
+    branch="master"
+fi
+
 # cleanup any existing daemon or venv
 sudo rm -f /etc/systemd/system/globus_cw_daemon.service
 sudo rm -f /etc/cwlogd.ini
@@ -11,20 +20,20 @@ rm -rf venv
 set -e
 
 # setup the daemon as root
-sudo pip install git+https://github.com/globus/globus-cwlogger#subdirectory=daemon
-sudo globus_cw_daemon_install cwlogger-test --stream_name test-stream
+sudo pip install git+https://github.com/globus/globus-cwlogger@$branch#subdirectory=daemon
+sudo globus_cw_daemon_install cwlogger-test --stream-name test-stream
 sudo systemctl daemon-reload
 sudo service globus_cw_daemon restart
 sudo service globus_cw_daemon status
 
 # python 2 client test in venv
 virtualenv venv
-venv/bin/pip install git+https://github.com/globus/globus-cwlogger#subdirectory=client
+venv/bin/pip install git+https://github.com/globus/globus-cwlogger@$branch#subdirectory=client
 venv/bin/python client_test.py
 rm -rf venv
 
 # python 3 client test in venv
 virtualenv venv --python=python3
-venv/bin/pip install git+https://github.com/globus/globus-cwlogger#subdirectory=client
+venv/bin/pip install git+https://github.com/globus/globus-cwlogger@$branch#subdirectory=client
 venv/bin/python client_test.py
 rm -rf venv
