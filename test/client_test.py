8a948e4517f6e336e5acdc78bffc17324172a9c3
Stephen Rosen <sirosen@globus.org>
2019-03-11 20:02:10 +0000

    Tweak test script
    - print what we're doing to make it more obvious that things are running
      when they're supposed to
    - ensure that the errors raised in the daemon-down tests are of the
      correct type
    
    closes #21

--- test/client_test.py
+++ test/client_test.py
@@ -10,18 +10,19 @@ except NameError:
 
 
 def main():
+    print("client_test.py")
 
-    # simple ascii test, confirms ascii str works for python2 and 3
+    print("  simple ascii test, confirms ascii str works for python2 and 3")
     log_event("Hello world from python%d!" % sys.version_info[0])
 
-    # non ascii bytes test
+    print("  non ascii bytes test")
     byte_string = (b"Non-ascii utf-8 bytes: "
                    b"\xe3\x83\x8f\xe3\x83\xad\xe3\x83\xbc\xe3\x83\xbb"
                    b"\xe3\x83\xaf\xe3\x83\xbc\xe3\x83\xab\xe3\x83\x89")
     assert isinstance(byte_string, bytes)
     log_event(byte_string)
 
-    # non ascii unicode test
+    print("  non ascii unicode test")
     unicode_string = byte_string.decode("utf-8").replace("bytes", "unicode")
     assert isinstance(unicode_string, UNICODE_TYPE)
     log_event(unicode_string)
