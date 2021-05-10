d34146bb1cc635c82b892370e6d3fc046ba24641
Stephen Rosen <sirosen@globus.org>
2021-05-10 20:16:56 +0000

    Update readme and tests to drop py2

--- test/client_test.py
+++ test/client_test.py
@@ -2,18 +2,11 @@ import sys
 
 from globus_cw_client.client import log_event
 
-try:
-    # Python 2
-    UNICODE_TYPE = unicode
-except NameError:
-    # Python 3
-    UNICODE_TYPE = str
-
 
 def main():
     print("client_test.py")
 
-    print("  simple ascii test, confirms ascii str works for python2 and 3")
+    print("  simple ascii test")
     log_event("Hello world from python%d!" % sys.version_info[0])
 
     print("  non ascii bytes test")
@@ -27,7 +20,7 @@ def main():
 
     print("  non ascii unicode test")
     unicode_string = byte_string.decode("utf-8").replace("bytes", "unicode")
-    assert isinstance(unicode_string, UNICODE_TYPE)
+    assert isinstance(unicode_string, str)
     log_event(unicode_string)
 
 
